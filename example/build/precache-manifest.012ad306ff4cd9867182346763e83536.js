18a6588fc5049126fe2f6437e328a507ea079d79
azi <a.robatjaiz560@gmail.com>
2021-09-27 21:21:46 +0330

    a

---
+++ example/build/precache-manifest.012ad306ff4cd9867182346763e83536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94c2e0bdf4e93c89b2ff2d59ac84f41",
+    "url": "./index.html"
+  },
+  {
+    "revision": "124631ae6797122dde07",
+    "url": "./static/css/main.afaaea28.chunk.css"
+  },
+  {
+    "revision": "762bbc4a2cf9c4caef1e",
+    "url": "./static/js/2.f003d6dd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f003d6dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "124631ae6797122dde07",
+    "url": "./static/js/main.d236a5c9.chunk.js"
+  },
+  {
+    "revision": "d6b05ebc4c1189e76a6b",
+    "url": "./static/js/runtime-main.36d89f99.js"
+  }
+]);
