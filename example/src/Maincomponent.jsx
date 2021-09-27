73595903e07fe99c66fe892e6eb2ca8de028c1fa
azi <a.robatjaiz560@gmail.com>
2021-09-27 20:30:03 +0330

    sumcpmp

--- example/src/Maincomponent.jsx
+++ example/src/Maincomponent.jsx
@@ -1,44 +1,96 @@
-import React, { Component } from 'react';
-import Subcomponent from './Subcomponent';
+import React, { Component } from 'react'
+import Subcomponent from './Subcomponent'
 class Maincomponent extends React.Component {
-    state={
-        Numbers:[],
-        count:0
+  constructor(props) {
+    super(props)
+  }
+  state = {
+    compArray: [],
+    count: 0,
+    sumArray: []
+  }
+  componentDidMount = () => {
+    this.CreateSubComponents(3)
+  }
+  handleSumChange = (id, sum) => {
+    const sumArray = [...this.state.sumArray]
+    sumArray[id] = sum
+    this.setState({ sumArray })
+  }
+  handelSumReduce = () => {
+    const sum = this.state.sumArray.reduce()
+  }
+
+  handleCountChange = (e) => {
+    this.setState({ count: e.target.value })
+    let param = parseInt(e.target.value)
+    this.CreateSubComponents(param)
+
+    console.log(this.state.Numbers)
+  }
+
+  CreateSubComponents(count) {
+    const compArray = []
+    const sumArray = []
+    for (let i = 0; i < count; i++) {
+      compArray.push(
+        <Subcomponent key={i} id={i} onSumChange={this.handleSumChange} />
+      )
+      sumArray.push(0)
     }
+    this.setState({ compArray, sumArray })
 
-    handelcount=(e)=>{
-        this.setState({count:e.target.value});   
-        const compArray=[];
-        let param=this.state.count;
-        console.log(param);
-        for (let i = 0; i < param; i++)
-            {  
-            
-                compArray.push({id:i,num1:0,num2:0,sum:0})
-                this.setState({Numbers:compArray});  
-                            
-            } 
-           
-            console.log(this.state.Numbers)
-        }
-        
-    render() { 
-        return <div>
-        
-            <h1 style={{color:"gray"}}>Count </h1>
-            <input style={{marginTop:0,height:20}} onChange={this.handelcount}type="text" placeholder="Type..."/>  
-            <div style={{display:"grid",gridTemplateColumns:"auto auto auto auto",gap:10,backgroundColor:"pink"}} >     
-            {this.state.Numbers.map(number=>
-
-            <Subcomponent  key={number.id} /> 
-
-
-
-            )}
-        </div>
-        
-    </div>
-    }
-}
+  }
+  computeSum=(array)=> {
+    const sum = array.reduce((total, number) => total + number, 0);
+    console.log("sum",sum);
+    return sum;
  
-export default Maincomponent;
+    //   console.log("arr",array)
+    //   let sum = 0
+    // for (let index = 0; index < array; index++) {
+       
+    //   sum += array[index]    
+  
+     
+    // }
+
+    //     return sum
+  
+    
+  }
+  render() {
+    return (
+      <div>
+        <label>{this.computeSum( this.state.sumArray )}</label>
+        <h1 style={{ color: 'gray' }}>Count </h1>
+        <input
+          onChange={this.handleCountChange}
+          type='number'
+          defaultValue='3'
+          min='0'
+          max='10'
+          placeholder='Type...'
+        />
+
+        <div
+          style={{
+            padding: '10px',
+            display: 'grid',
+            gridTemplateColumns: 'auto auto auto auto',
+            gap: '10px',
+            fontWeight: 'bold',
+            backgroundColor: 'pink'
+          }}
+        >
+          {/* {this.state.Numbers.map((number) => (
+            <Subcomponent key={number.id} />
+          ))} */}
+          {this.state.compArray}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Maincomponent
--- example/src/Subcomponent.jsx
+++ example/src/Subcomponent.jsx
@@ -1,39 +1,63 @@
 import React, { Component } from 'react'
 class Subcomponent extends React.Component {
-    state={
-        Number:[
-            {
-                num1:0,
-                num2:0                
-            }  
-        ]
-    }
-    render() { 
-        return <div >
-            
-            <div style={{display:'grid' ,placeItems:"center",   
-        
-        
-        
-        
-        
-        
-        }}>
-                <h3 style={{color:"gray"}}>Insert Two Number</h3>
-            <input placeholder="Enter Number1" onChange={e=>this.setState({num1:e.target.value})}type="text"  />
-            <br/>
-            <input placeholder="Enter Number2" onChange={e=>this.setState({num2:e.target.value})}type="text"  />
-            <br/>
-            <label style={{color:"blue"}}><b><span style={{color:"gray"}} >SUM</span> :{parseInt(this.state.num1)+parseInt(this.state.num2)} </b> </label>
-           <hr/>
-           </div>
+  state = {
+    Number: [
+      {
+        num1: 0,
+        num2: 0,  
+        sum:0         
+      }        
+    ]
+  }
+  handleChange1=(e)=>{
+    this.setState({ num1:e.target.value });
+    this.props.onSumChange(this.props.id,parseInt(this.state.num2)+parseInt(e.target.value))
+  }
+  handleChange2=(e)=>{
+    this.setState({ num2:e.target.value });
+    this.props.onSumChange(this.props.id,parseInt(this.state.num1)+parseInt(e.target.value))
+  }
+  
+  render() {
+    const sumnum=parseInt(this.state.num1)+parseInt(this.state.num2);
+    
+    console.log(sumnum)
 
-            
+    return (
+       
+            <div style={{backgroundColor:"lightgreen",padding:"10px"}}>
 
-            
-      
-        </div>;
-    }
+                <div style={{ display: 'grid', placeItems: 'center' }}>
+                <h3 style={{ color: 'gray' }}>Insert Two Number</h3>
+                <input
+                placeholder='Enter Number1'
+                onChange={this.handleChange1}
+                type='text'
+                    />
+
+                <br />
+                <input
+                    placeholder='Enter Number2'                  
+                    onChange={this.handleChange2}
+                    // onChange={(e) =>{this.setState({ num2: e.target.value })}}
+                    type='text'
+                />
+                <br />
+                <label style={{ color: 'blue' }}>
+                        <b>
+                        <span 
+                        ref={this.span}style={{ color: 'gray' }}>SUM</span> :
+                        {/* {sumnum} {this.props.onSumChange(this.props.id,sumnum)} */}
+                        {isNaN(sumnum)?parseInt(0):sumnum} {this.props.onSumChange(this.props.id,isNaN(sumnum)?parseInt(0):sumnum)}
+                        {/* {isNaN(parseInt(this.state.num1) + parseInt(this.state.num2))?"---":parseInt(this.state.num1) + parseInt(this.state.num2)} */}
+                        {/* {this.state.sumStr} */}
+                        </b>
+                </label>
+                <hr />
+                </div>
+      </div>
+    )
+  }
 }
- 
-export default Subcomponent;
+
+export default Subcomponent
